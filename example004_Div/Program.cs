1a4670433c0c286b5173d3b649328cfe22bb0916
omezhka <murasakitta@gmail.com>
2022-08-04 09:36:04 +0300

    Добавили комментарии

--- example004_Div/Program.cs
+++ example004_Div/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = 73;
+﻿// double - вещественные числа, int - целые числы, string - строки, bool - логический
+
+double numberA = 73;
 double numberB = 3;
 double result = numberA / numberB;
 Console.WriteLine(result);
