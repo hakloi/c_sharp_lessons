1dcb4cafe32aaba4bdb116ee96ccfcf64b39d7c1
omezhka <murasakitta@gmail.com>
2022-08-04 09:41:17 +0300

    Добавили операцию с рандомными числами

--- example004_Div/Program.cs
+++ example004_Div/Program.cs
@@ -1,6 +1,13 @@
 ﻿// double - вещественные числа, int - целые числы, string - строки, bool - логический
 
+// Используем деление, вещественные числа
 double numberA = 73;
 double numberB = 3;
 double result = numberA / numberB;
-Console.WriteLine(result);
+Console.WriteLine(numberA / numberB);
+
+//Используем операцию с рандомными числами
+int number1 = new Random().Next(1, 87);
+int number2 = new Random().Next(4, 45);
+int sum = number1 + number2;
+Console.WriteLine(sum);
