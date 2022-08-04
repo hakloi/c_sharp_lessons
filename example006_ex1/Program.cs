841ea85a67a371df572d5d3f617c9486f711aceb
omezhka <murasakitta@gmail.com>
2022-08-04 09:55:57 +0300

    Нахождение максимального числа

---
+++ example006_ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 5;
+int c = 16;
+int d = 42;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
