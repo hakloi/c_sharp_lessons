f6ca32c16e28a24691076bc4d30f6e2afe1ed6d6
omezhka <murasakitta@gmail.com>
2022-08-21 17:52:38 +0300

    Таблица квадратов

---
+++ seminar22/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 22:
+Напишите программу, которая принимает на вход число (N)
+и выдвет таблицу квадратов чисел от 1 до N.
+*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i < number | i == number)
+{
+    Console.WriteLine(Math.Pow(i, 2));
+    i++;
+}
+
+
