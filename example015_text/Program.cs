912d5d5dc4af0a05483332db7c6c24a946e9c4b0
omezhka <murasakitta@gmail.com>
2022-08-23 11:17:37 +0300

    Изменение текста

---
+++ example015_text/Program.cs
@@ -0,0 +1,22 @@
+﻿string text = "\n Когда же юности мятежной \n"
++ "Пришла Евгению пора, \n"
++ "Пора надежд и грусти нежной, \n"
++ "Monsieur прогнали со двора. \n";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '_');
+
+
+Console.WriteLine(newText);
