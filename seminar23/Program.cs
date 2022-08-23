b9d87b67affb86c602842b1b84dc2cff7addbda5
omezhka <murasakitta@gmail.com>
2022-08-23 20:01:06 +0300

    Сумма числа от 1 до N

---
+++ seminar23/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int GetSum(int num)
+{
+    int result = 0;
+    for (int i = 0; i <= num; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+int Sum = GetSum(number);
+Console.WriteLine($"Получими: {Sum}");
