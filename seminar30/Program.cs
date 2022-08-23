861650f14a59c48ad021557b05ff3c95e38fe860
omezhka <murasakitta@gmail.com>
2022-08-23 21:37:31 +0300

    Рандомный массив с 0 и 1

---
+++ seminar30/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = new int[8];
+
+int[] RandomArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+
+int[] res = RandomArray(array);
+PrintArray(res);
