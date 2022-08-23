e15b6a6b06837d3f007c7dca8a3af5da448097fc
omezhka <murasakitta@gmail.com>
2022-08-23 20:27:28 +0300

    Счетчик количества цифор в числе

---
+++ seminar26/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int HowManyNum(int num)
+{
+    string str = num.ToString();
+    int result = str.Length;
+    
+    return result;
+}
+
+int res = HowManyNum(number);
+Console.WriteLine(res);
