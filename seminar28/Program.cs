773fb60d8b49b9507ce870dfd1bdc3d0ee7f48e7
omezhka <murasakitta@gmail.com>
2022-08-23 21:07:54 +0300

    Факториал числа

---
+++ seminar28/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int GetFactorial(int num)
+{
+    int result = 1;
+    if (num == 1)
+    {
+        return 1;
+    }
+
+    for (int i = 2; i <= num; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+int res = GetFactorial(number);
+Console.WriteLine($"Из числа {number}, получили факториал этого числа, равный =  {res}.");
