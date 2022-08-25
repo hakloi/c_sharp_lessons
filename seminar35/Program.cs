7ccc0115dbc9f7d12f0f109b7a39a0ea9043975d
omezhka <murasakitta@gmail.com>
2022-08-25 13:29:18 +0300

    Рандомный массив

---
+++ seminar35/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+int[] array = new int[123];
+
+int[] RandomArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 200);
+    }
+    return arr;
+}
+
+int[] PrintArray(int[] arr)
+{
+    string text = "\nПолучили новый массив: ";
+    Console.WriteLine(text);
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] <= 99 & arr[i] >= 10)
+        {
+            count++;
+            Console.Write($"{arr[i]} ");
+        }
+    }
+    return arr;
+}
+
+int[] res = RandomArray(array);
+PrintArray(res);
