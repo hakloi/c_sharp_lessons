3483ac187277dd34b11f6218e433f8feaa9564b0
omezhka <murasakitta@gmail.com>
2022-08-25 15:24:17 +0300

    Произведение соседних элементов массива

---
+++ seminar36/Program.cs
@@ -0,0 +1,42 @@
+﻿int[] array = new int[6];
+
+int[] GetArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 10);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] res = GetArray(array);
+string text = "\nМассив: ";
+Console.Write(text);
+PrintArray(res);
+
+int[] MultiplicationArray (int[] arr)
+{
+    int[] newArray = new int[arr.Length/2];
+    for (int i = 0; i < arr.Length/2; i++)
+    {
+        newArray[i] = arr[i] * arr[arr.Length-i-1];
+        if (arr[i] == arr[arr.Length-i-1])
+        {
+            break;
+        }
+    }
+    return newArray;
+}
+
+int[] newRes = MultiplicationArray(array);
+string textNew = "\nНовый массив: ";
+Console.Write(textNew);
+PrintArray(newRes);
