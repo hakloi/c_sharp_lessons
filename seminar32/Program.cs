b72267c11ac8d9740f03fe967ec1c3ae3009552c
omezhka <murasakitta@gmail.com>
2022-08-25 11:18:49 +0300

    Меняет знаки на противоположенные

---
+++ seminar32/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = new int[4]{-4,-8,8,2};
+
+int[] ChangeSymbols(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i]*(-1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] result = ChangeSymbols(array);
+PrintArray(result);
