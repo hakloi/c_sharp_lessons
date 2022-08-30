fca4109332b22ebba882ac4cdd9e11e5e52470dd
omezhka <murasakitta@gmail.com>
2022-08-30 20:00:34 +0300

    Перевернутый массив

---
+++ seminar39/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+
+int[] array = new int[7];
+
+int[] RandomArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 21);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("Массив: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] randomArray = RandomArray(array);
+PrintArray(randomArray);
+
+int[] TurnOverArray(int[] array)
+{
+    int[] result = new int[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[array.Length - 1 - i];
+    }
+    return result;
+}
+
+int[] turnOver = TurnOverArray(randomArray);
+string text = "\nПеревернутый ";
+Console.Write(text);
+PrintArray(turnOver);
