7441ab0790b73310eae324a8604dfb980d14a5f8
omezhka <murasakitta@gmail.com>
2022-08-30 20:22:34 +0300

    Существует ли треугольник?

---
+++ seminar40/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.Write("Значение первой стороны треугольника: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Значение второй стороны треугольника: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Значение третьей стороны треугольника: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+void TriagleExists (int a, int b, int c)
+{
+    if (a + b > c && b + c > a && c + a > b) 
+    {
+        Console.Write($"Треугольник со значениями: {a}, {b}, {c} - может существовать!");
+    }
+    else
+    {
+        Console.Write($"Треугольник со значениями: {a}, {b}, {c} - не может существовать!");
+    }
+}
+
+TriagleExists(a, b, c);
