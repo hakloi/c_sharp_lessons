ab156f6991a737dc572b0466ba125fb322758324
omezhka <murasakitta@gmail.com>
2022-08-30 16:53:11 +0300

    Двумерный массив!

---
+++ example17_matrix/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+int[,] matrix = new int[3, 4];
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,21);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+string text = "Массив:\n";
+Console.Write(text);
+PrintArray(matrix);
+FillArray(matrix);
+
+Console.Write(text);
+PrintArray(matrix);
