2178c2014d2559db2168e846761e041f44737866
omezhka <murasakitta@gmail.com>
2022-09-01 09:02:51 +0300

    Перевод в двоичную систему

---
+++ seminar42/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string ConvertBinary(int N)
+{
+    string num = String.Empty;
+    while (N > 0)
+    {
+        num = N % 2 + num;
+        N = N / 2;
+    }
+    return num;
+}
+
+string result = ConvertBinary(number);
+Console.WriteLine(result);
