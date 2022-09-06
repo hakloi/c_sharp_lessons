22f7ab7fd5be7e9ac9cf931795a29d3d67081924
omezhka <murasakitta@gmail.com>
2022-09-06 20:57:03 +0300

    Меняем последнюю и первую строчки

---
+++ seminar53/Program.cs
@@ -0,0 +1,66 @@
+﻿Console.Clear();
+
+Console.Write("Введите количество строк: ");
+bool isNumberM = int.TryParse(Console.ReadLine(), out int m);
+
+Console.Write("Введите количество столбцов: ");
+bool isNumberN = int.TryParse(Console.ReadLine(), out int n);
+
+if (isNumberM == false || isNumberN == false)
+{
+    Console.WriteLine("Введены неправильные данные.");
+    Console.Write("Введите число!");
+    return;
+}
+
+int[,] FillArray2D(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    Random random = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = random.Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+int[,] result = FillArray2D(m, n);
+
+void PrintArray2D(int[,] arr)
+{
+    Console.WriteLine();
+    Console.Write("Массив: ");
+    Console.WriteLine();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray2D(result);
+
+int[,] ChangeFirstToLast(int[,] arr)
+{
+    Console.WriteLine();
+    Console.WriteLine("Меняем местами первую и последнюю строки массива!");
+    int temp = 0;
+    int rows = arr.GetLength(0);
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        temp = arr[0, i];
+        arr[0, i] = arr[rows - 1, i];
+        arr[rows - 1, i] = temp;
+    }
+    return arr;
+}
+
+int[,] res = ChangeFirstToLast(result);
+PrintArray2D(res);
