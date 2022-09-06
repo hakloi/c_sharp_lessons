5450ed3e79792fa7b35d856446f0f7055b380387
omezhka <murasakitta@gmail.com>
2022-09-06 21:19:10 +0300

    Заменим строки на столбцы в массиве

---
+++ seminar55/Program.cs
@@ -0,0 +1,75 @@
+﻿Console.Clear();
+
+Console.Write("Введите количество строк: ");
+bool isNumberM = int.TryParse(Console.ReadLine(), out int m);
+
+Console.Write("Введите количество столбцов: ");
+bool isNumberN = int.TryParse(Console.ReadLine(), out int n);
+
+if (isNumberM == false || isNumberN == false)
+{
+    Console.WriteLine("Введены неправильные данные.");
+    Console.Write("Введите число!");
+    return;
+}
+
+int[,] FillArray2D(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    Random random = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = random.Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+int[,] result = FillArray2D(m, n);
+
+void PrintArray2D(int[,] arr)
+{
+    Console.WriteLine();
+    Console.Write("Массив: ");
+    Console.WriteLine();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray2D(result);
+
+
+if (m != n)
+{
+    Console.WriteLine();
+    Console.WriteLine("Невозможно заменить строку на столбец, так как массив не квадрат!");
+    Console.Write("Введите другие данные!");
+    return;
+}
+
+int[,] ChangeRowsColums(int[,] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = i+1; j < array.GetLength(1); j++)
+        {
+            temp = array[i,j];
+            array[i,j] = array[j,i];
+            array[j,i] = temp;
+        }
+    }
+    return array;
+}
+
+int[,] res = ChangeRowsColums(result);
+PrintArray2D(res);
